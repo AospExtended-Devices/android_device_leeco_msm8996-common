5367fcc65d8f13b141b8092c0dee2fc49ae23487
shivatejapeddi <shivatejapeddi@gmail.com>
2019-07-02 09:57:15 +0000

    msm8996 : Increase Boost Values

--- msm8996.mk
+++ msm8996.mk
@@ -193,6 +193,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Doze
 
+# Fingerprint sensor
+PRODUCT_PACKAGES += \
+    fingerprintd \
+    fingerprint.msm8996
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
